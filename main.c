cad12ba90f9c091b4fb9ec8aa8fa158cd4fcd433
Bibounet <bibou.ytb33@gmail.com>
2026-06-09 14:12:37 +0200

    added basic menu struct

--- main.c
+++ main.c
@@ -2,4 +2,19 @@
 
 int main() {
     printf("hi!");
+
+    int choice = 0;
+
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("1");
+            break;
+        case 2:
+            printf("2");
+            break;
+    }
+
+
 }
