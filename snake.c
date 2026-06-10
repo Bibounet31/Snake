abc7e1fd8813ba7b886d7cbf4d3ed2533234d9fe
Bibounet <bibou.ytb33@gmail.com>
2026-06-10 13:42:02 +0200

    memory leak fix

--- snake.c
+++ snake.c
@@ -24,27 +24,26 @@ void move_snake(int **grid, Snake *snake) {
         case 3: new_head.y--; break; // left
     }
 
-
     // if food > grow
-    if (grid[new_head.x][new_head.y] == 2) {
-        snake->length++;
-        snake->body = realloc(snake->body, snake->length * sizeof(Point));
-        spawn_food(grid);
-    } else {
-        // remove old tail position
-        grid[snake->body[snake->length - 1].x][snake->body[snake->length - 1].y] = 0;
+    if (new_head.x >= 0 && new_head.x < ROWS && new_head.y >= 0 && new_head.y < COLS) {
+        if (grid[new_head.x][new_head.y] == 2) {
+            snake->length++;
+            snake->body = realloc(snake->body, snake->length * sizeof(Point));
+            spawn_food(grid);
+        } else {
+            // remove old tail position
+            grid[snake->body[snake->length - 1].x][snake->body[snake->length - 1].y] = 0;
+        }
     }
-
     // move each segment to the one next to it (tail to head)
     for (int i = snake->length - 1; i > 0; i--) {
         snake->body[i] = snake->body[i - 1];
     }
-
     // set new head pos qwq
     snake->body[0] = new_head;
-    grid[new_head.x][new_head.y] = 1;
+    if (new_head.x >= 0 && new_head.x < ROWS && new_head.y >= 0 && new_head.y < COLS)
+        grid[new_head.x][new_head.y] = 1;
 }
-
 
 
 int check_collision(int **grid, Snake *snake) {
